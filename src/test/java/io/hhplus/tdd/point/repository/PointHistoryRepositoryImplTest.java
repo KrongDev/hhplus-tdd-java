8f87685e4185e04793e2a571539f8568f18637e6
Geon Lee <drik86421@gmail.com>
2024-06-17 21:46:08 +0900

    fix: import clean

--- src/test/java/io/hhplus/tdd/point/repository/PointHistoryRepositoryImplTest.java
+++ src/test/java/io/hhplus/tdd/point/repository/PointHistoryRepositoryImplTest.java
@@ -8,12 +8,11 @@ import jdk.jfr.Description;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @WebMvcTest({PointHistoryRepositoryImpl.class, PointHistoryTable.class})
 class PointHistoryRepositoryImplTest {
