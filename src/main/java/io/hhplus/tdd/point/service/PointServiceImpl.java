3256b9d70befa35ea11a2b34ac086cfa16a2353a
KrongDev <leegeondev@gmail.com>
2024-06-20 03:18:55 +0900

    fix: Injected the Repository into the Service Layer.

--- src/main/java/io/hhplus/tdd/point/service/PointServiceImpl.java
+++ src/main/java/io/hhplus/tdd/point/service/PointServiceImpl.java
@@ -2,6 +2,8 @@ package io.hhplus.tdd.point.service;
 
 import io.hhplus.tdd.point.aggregate.domain.PointHistoryDomain;
 import io.hhplus.tdd.point.aggregate.domain.UserPointDomain;
+import io.hhplus.tdd.point.repository.PointHistoryRepository;
+import io.hhplus.tdd.point.repository.UserPointRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,33 +13,34 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PointServiceImpl implements PointService{
     //
+    private final UserPointRepository userPointRepository;
+    private final PointHistoryRepository pointHistoryRepository;
+
     public UserPointDomain loadPoint(long userId) {
-        UserPointDomain userPointDomain = UserPointDomain.sample();
-        userPointDomain.setId(userId);
-        return userPointDomain;
+        return this.userPointRepository.findById(userId);
     }
 
     public List<PointHistoryDomain> loadHistory(long userId) {
-        return List.of();
+        return this.pointHistoryRepository.findAllByUserId(userId);
     }
 
     public UserPointDomain chargePoint(long userId, long chargePoint) {
         if(chargePoint <= 0)
             throw new RuntimeException("충전하려는 금액이 이상합니다");
-        UserPointDomain userPointDomain = UserPointDomain.sample();
-        userPointDomain.setId(userId);
+        UserPointDomain userPointDomain = this.userPointRepository.findById(userId);
         userPointDomain.chargePoint(chargePoint);
+        this.userPointRepository.save(userPointDomain.getId(), userPointDomain.getPoint());
         return userPointDomain;
     }
 
     public UserPointDomain usePoint(long userId, long usePoint) {
-        UserPointDomain userPointDomain = UserPointDomain.sample();
-        userPointDomain.setId(userId);
+        UserPointDomain userPointDomain = this.userPointRepository.findById(userId);
         if(usePoint <= 0)
             throw new RuntimeException("사용하려는 포인트액수가 이상합니다.");
         if(userPointDomain.isPointInsufficient(usePoint))
             throw new RuntimeException("잔액이 부족합니다");
         userPointDomain.usePoint(usePoint);
+        this.userPointRepository.save(userPointDomain.getId(), userPointDomain.getPoint());
         return userPointDomain;
     }
 }
--- src/test/java/io/hhplus/tdd/point/service/PointServiceImplTest.java
+++ src/test/java/io/hhplus/tdd/point/service/PointServiceImplTest.java
@@ -1,18 +1,19 @@
 package io.hhplus.tdd.point.service;
 
-import io.hhplus.tdd.point.aggregate.domain.PointHistoryDomain;
 import io.hhplus.tdd.point.aggregate.domain.UserPointDomain;
+import io.hhplus.tdd.point.repository.PointHistoryRepository;
+import io.hhplus.tdd.point.repository.UserPointRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.annotation.Description;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PointServiceImplTest {
@@ -20,38 +21,22 @@ class PointServiceImplTest {
     @InjectMocks
     PointServiceImpl pointService;
 
-    @Test
-    @DisplayName("포인트 조회")
-    void loadPoint() {
-        //Given
-        long userId = 1;
-        //When
-        UserPointDomain userPoint = pointService.loadPoint(userId);
-        //Then
-        assertNotNull(userPoint);
-        assertEquals(userId, userPoint.getId());
-    }
-
-    @Test
-    @DisplayName("포인트 조회")
-    void loadHistory() {
-        //Given
-        long userId = 1;
-        //When
-        List<PointHistoryDomain> pointHistory = pointService.loadHistory(userId);
-        //Then
-        assertNotNull(pointHistory);
-    }
+    @Mock
+    private PointHistoryRepository pointHistoryRepository;
+    @Mock
+    private UserPointRepository userPointRepository;
 
     @Test
     @DisplayName("포인트 충전")
-    @Description("포인트 충전시 0원이거나 0원 이하의 경우 에러 발생")
+    @Description("포인트 충전이 정상적으로 수행되는지 테스트")
     void chargePoint() {
         //Given
         long userId = 1;
         long chargePoint = 100;
+        when(userPointRepository.findById(userId)).thenReturn(new UserPointDomain(userId, 0, 0));
         UserPointDomain prevUserPoint = pointService.loadPoint(userId);
         //When
+        when(userPointRepository.findById(userId)).thenReturn(new UserPointDomain(userId, 0, 0));
         UserPointDomain userPoint = pointService.chargePoint(userId, chargePoint);
         //Then
         assertNotNull(userPoint);
@@ -61,13 +46,15 @@ class PointServiceImplTest {
 
     @Test
     @DisplayName("포인트 사용")
-    @Description("잔여 포인트보다 사용하려는 포인트가 많거나 사용하려는 포인트가 0원이면 에러 발생")
+    @Description("포인트가 정상적으로 사용되는지 테스트")
     void usePoint() {
         //Given
         long userId = 1;
-        long usePoint = 0;
+        long usePoint = 10;
+        when(userPointRepository.findById(userId)).thenReturn(new UserPointDomain(userId, 100, 0));
         UserPointDomain prevUserPoint = pointService.loadPoint(userId);
         //When
+        when(userPointRepository.findById(userId)).thenReturn(new UserPointDomain(userId, 100, 0));
         UserPointDomain userPoint = pointService.usePoint(userId, usePoint);
         //Then
         assertNotNull(userPoint);
